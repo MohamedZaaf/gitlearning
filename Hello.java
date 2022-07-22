1f7817beaa1ee755a74672660adb653e66bde564
Zaaf Mohamed <zaafmohamed475@gmail.com>
2022-07-22 12:27:15 +0100

    Hello from feature branch

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
     public static void main(String[] args) {
-    System.out.println("Hello world");
-    System.out.println("another ");
+    System.out.println("Hello from feature branch");
 }
 }
